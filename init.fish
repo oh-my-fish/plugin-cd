a800a99f6129e2d5a2be4cad5a0e028c7b25b7fb
Jorge Bucaran <mail@jorgebucaran.com>
2018-10-07 14:39:27 +0900

    fix: don't source function file; __plugin_cd is always defined; fix #8

--- init.fish
+++ init.fish
@@ -5,8 +5,4 @@ if not functions -q __plugin_cd_wrapped_cd
   functions -e cd
 end
 
-if not functions -q __plugin_cd
-  source $SOURCE_DIR/functions/plugin-cd.fish
-end
-
 functions -c __plugin_cd cd
