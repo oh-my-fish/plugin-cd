9d808d8b9775bcb731c501fdd0b16f6081865996
Jorge Bucaran <mail@jorgebucaran.com>
2018-10-07 14:36:55 +0900

    refactor: remove irrelevant sourcing of symbolic links in source dir

--- init.fish
+++ init.fish
@@ -1,10 +1,4 @@
-set SOURCE_FILE (status -f)
-# For fishman symlink
-while test -L $SOURCE_FILE
-  set SOURCE_FILE (readlink $SOURCE_FILE)
-end
-
-set SOURCE_DIR (dirname $SOURCE_FILE)
+set SOURCE_DIR (dirname (status -f))
 
 if not functions -q __plugin_cd_wrapped_cd
   functions -c cd __plugin_cd_wrapped_cd
